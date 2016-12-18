2fef26b5385190d1c0556af466db7e24b98a4482
Nelma <nelmasb@gmail.com>
2016-12-18 10:01:43 -0200

    teste unitario com karma e jasmine

---
+++ capitulo3/controllerSpec.js
@@ -0,0 +1,34 @@
+describe('Controller: ListCtrl', function(){
+
+    //instancia uma nova versao do modulo antes de cada teste
+    beforeEach(module('notesApp'));
+
+    var ctrl;
+
+    //antes de cada teste de unidade, cria uma nova instancia do controlador
+    //$controller eh um servico do Angular
+    beforeEach(inject(function($controller){
+        ctrl = $controller('ListCtrl');
+    }));
+
+    it('should have items available on load', function(){
+        expect(ctrl.items).toEqual([
+            {id: 1, label: 'First', done: true},
+            {id: 2, label: 'Second', done: false}
+        ]);
+    });
+
+    it('should have highlight items bases on state', function(){
+        var item = {id: 1, label: 'First', done: true};
+        var actualClass = ctrl.getDoneClass(item);
+
+        expect(actualClass.finished).toBeTruthy();
+        expect(actualClass.unfinished).toBeFalsy();
+
+        item.done = false;
+
+        actualClass = ctrl.getDoneClass(item);
+        expect(actualClass.finished).toBeFalsy();
+        expect(actualClass.unfinished).toBeTruthy();
+    })
+});
--- capitulo3/karma.conf.js
+++ capitulo3/karma.conf.js
@@ -1,15 +1,16 @@
 module.exports = function(config) {
     config.set({
         basePath: '',
-        framework: ['jasmine'],
-        files: [
-            'angular.min.js',
-            'angular-mock.js',
+        framework:['jasmine'],
+        files:[
+            'node_modules/karma-jasmine/lib/jasmine.js',
+            'node_modules/karma-jasmine/lib/adapter.js',
+            'node_modules/angular/angular.min.js',
+            'node_modules/angular-mocks/angular-mocks.js',
             'controller.js',
-            'simpleSpec.js',
             'controllerSpec.js'
         ],
-        exclude: [],
+        exclude:[],
         port: 8080,
         logLevel: config.LOG_INFO,
         autoWatch: true,
