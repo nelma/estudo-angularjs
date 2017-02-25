17cf9a924f08fc2882445ed820da53909104c6d1
Nelma <nelmasb@gmail.com>
2017-02-25 18:40:53 -0300

    exemplo de app utilizando ngRoute

---
+++ capitulo10/routing-example/app/scripts/controllers.js
@@ -0,0 +1,43 @@
+angular.module('fifaApp')
+    .controller('MainCtrl', ['UserService', function(UserService) {
+        var self = this;
+        self.userService = UserService;
+
+        //Verifica se o usr esta logado qdo a app for carregada
+        //UserService ira atualizar isLoggedIn automaticamente
+        //depois que esta chamada for concluida
+
+        UserService.session();
+    }])
+    .controller('TeamListCtrl', ['FifaService', function(FifaService) {
+        var self = this;
+        self.teams = [];
+
+        FifaService.getTeams().then(function(resp) {
+            self.teams = resp.data;
+        });
+    }])
+    .controller('LoginCtrl', ['UserService', '$location', function(UserService, $location) {
+        var self = this;
+        self.user = {username: '', password: ''};
+
+        self.login = function() {
+            UserService.login(self.user).then(function(success) {
+                $location.path('/team');
+            }, function(error) {
+                self.errorMessage = error.data.msg;
+            })
+        }
+    }])
+    .controller('TeamDetailsCtrl',
+        ['$location', '$routeParams', 'FifaService',
+            function($location, $routeParams, FifaService) {
+                var self = this;
+                self.team = {};
+                FifaService.getTeamDetails($routeParams.code)
+                    .then(function(resp) {
+                        self.team = resp.data;
+                    }, function(error) {
+                        $location.path('/login');
+                    });
+    }]);
